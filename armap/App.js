ef05d79cf78e6c5a6656edd2ac64d7f4aeb561f3
Arthur Silverstein(bluegoo192) <silversteinarthur@gmail.com>
2018-01-20 14:06:35 -0800

    FIXED

--- armap/App.js
+++ armap/App.js
@@ -9,10 +9,34 @@ export default class App extends React.Component {
     return (
       <Expo.GLView
         style={{ flex: 1 }}
+        onContextCreate={this._onGLContextCreate}
       />
     );
   }
+
+  _onGLContextCreate = async (gl) => {
+   // Do graphics stuff here!
+   const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  75, gl.drawingBufferWidth / gl.drawingBufferHeight, 0.1, 1000);
+
+const renderer = ExpoTHREE.createRenderer({ gl });
+renderer.setSize(gl.drawingBufferWidth, gl.drawingBufferHeight);
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+camera.position.z = 5;
+const animate = () => {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+  gl.endFrameEXP();
 }
+animate();
+ }
+}
+
 
 const styles = StyleSheet.create({
   container: {
