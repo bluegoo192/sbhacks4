24e31fb90514be8d25bc53cf1262aa695d47495c
Michael Qu <Mzqu@users.noreply.github.com>
2018-01-20 16:30:50 -0800

    firebase start

--- armap/App.js
+++ armap/App.js
@@ -1,54 +1,129 @@
+import * as firebase from 'firebase';
 import React from 'react';
 import * as THREE from 'three';
 import ExpoTHREE from 'expo-three';
 import Expo from 'expo';
-import { StyleSheet, Text, View } from 'react-native';
+import { Constants, Location, Permissions } from 'expo';
+import { StyleSheet, Text, View, Button, PanResponder } from 'react-native';
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyAOh85GjLfS7opOpKaCMWXb9oqS0XhNkeM",
+  authDomain: "sbhacks4.firebaseapp.com",
+  databaseURL: "https://sbhacks4.firebaseio.com",
+  storageBucket: "sbhacks4.appspot.com",
+  messagingSenderId: "729739022598"
+};
+
+firebase.initializeApp(firebaseConfig);
 
 console.disableYellowBox = true;
 
 export default class App extends React.Component {
+  state = {
+    location: null,
+    cube: null,
+    camera: null
+  }
+
   render() {
     return (
-      <Expo.GLView
-        ref={(ref) => this._glView = ref}
-        style={{ flex: 1 }}
-        onContextCreate={this._onGLContextCreate}
-      />
+        <Expo.GLView
+          {...this.panResponder.panHandlers}
+          ref={(ref) => this._glView = ref}
+          style={{ flex: 1 }}
+          onContextCreate={this._onGLContextCreate}
+        />
     );
   }
 
+  componentWillMount() {
+    this.panResponder = PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onPanResponderGrant: () => {
+        // this.state.cube.position.x = this.state.camera.position.x;
+        // this.state.cube.position.y = this.state.camera.position.y;
+        // this.state.cube.position.z = this.state.camera.position.z - 0.1;
+        this.setSign("oh yoyoyo");
+      },
+      onPanResponderRelease: () => {
+      },
+      onPanResponderTerminate: () => {
+      },
+      onShouldBlockNativeResponder: () => false,
+    });
+    this._getLocationAsync();
+  }
+
+  _getLocationAsync = async () => {
+    var app = this;
+    let { status } = await Permissions.askAsync(Permissions.LOCATION);
+    if (status !== 'granted') {
+      this.setState({
+        errorMessage: 'Permission to access location was denied',
+      });
+    }
+
+    var location = await Location.getCurrentPositionAsync({
+      enableHighAccuracy: true
+    });
+    app.setState({ location: location });
+    console.log(location);
+  };
+
+  setSign = async (text) => {
+    Location.getCurrentPositionAsync({
+      enableHighAccuracy: true
+    }).then(function(location) {
+      // Location.getHeadingAsync().then(function(heading)) {
+      //
+      // }
+      var id = location.coords.latitude.toString().replace(".", "-") + "&" + location.coords.longitude.toString().replace(".", "-") + "&" + location.coords.altitude.toString().replace(".", "-");
+      console.log(id);
+      firebase.database().ref('signs/' + id).set({
+        text: text,
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+        altitude: location.coords.altitude,
+      });
+    })
+  }
+
+
   _onGLContextCreate = async (gl) => {
-   // Do graphics stuff here!
-   const arSession = await this._glView.startARSessionAsync();
-   const scene = new THREE.Scene();
-const camera = ExpoTHREE.createARCamera(
-  arSession,
-  gl.drawingBufferWidth,
-  gl.drawingBufferHeight,
-  0.01,
-  1000
-);
-const renderer = ExpoTHREE.createRenderer({ gl });
-renderer.setSize(gl.drawingBufferWidth, gl.drawingBufferHeight);
+    // Do graphics stuff here!
+    const arSession = await this._glView.startARSessionAsync();
+    const scene = new THREE.Scene();
+    const camera = ExpoTHREE.createARCamera(
+      arSession,
+      gl.drawingBufferWidth,
+      gl.drawingBufferHeight,
+      0.01,
+      1000
+    );
+    const renderer = ExpoTHREE.createRenderer({ gl });
+    renderer.setSize(gl.drawingBufferWidth, gl.drawingBufferHeight);
 
-scene.background = ExpoTHREE.createARBackgroundTexture(arSession, renderer);
+    scene.background = ExpoTHREE.createARBackgroundTexture(arSession, renderer);
 
-const geometry = new THREE.BoxGeometry(0.07, 0.07, 0.07);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-cube.position.z = -0.4;
-scene.add(cube);
+    const geometry = new THREE.BoxGeometry(0.07, 0.07, 0.02);
+    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    const cube = new THREE.Mesh(geometry, material);
+    cube.position.z = -0.4;
+    scene.add(cube);
+    this.setState({
+      cube: cube,
+      camera: camera
+    });
 
-cube.position.z = -0.4;
-const animate = () => {
-  requestAnimationFrame(animate);
-  cube.rotation.x += 0.07;
-cube.rotation.y += 0.04;
-  renderer.render(scene, camera);
-  gl.endFrameEXP();
-}
-animate();
- }
+    const animate = () => {
+      requestAnimationFrame(animate);
+      this.state.cube.rotation.y += 0.04;
+      renderer.render(scene, camera);
+      gl.endFrameEXP();
+    }
+    animate();
+  }
 }
 
 
@@ -59,4 +134,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  create: {
+    width: '100%',
+    height: '100%',
+    backgroundColor: 'red',
+    zIndex: 100,
+  }
 });
